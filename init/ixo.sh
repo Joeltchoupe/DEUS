d28cd0b6cd4b0b4db38bfa6024f031bb3cea48c0
WIA <131862530+Joeltchoupe@users.noreply.github.com>
2024-01-03 14:48:54 +0100

    Create ixo.sh

---
+++ init/ixo.sh
@@ -0,0 +1,13 @@
+# Créer un répertoire pour le projet
+mkdir Ixogen
+
+# Accéder au répertoire du projet
+cd Ixogen
+
+# Initialiser un nouveau dépôt Git
+git init
+
+# Créer une structure de dossiers personnalisée
+mkdir src
+mkdir tests
+touch README.md
